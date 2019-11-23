5e7d9038643bf5231b3ac906abe20e67de1ae33e
Dr. Maxim Orlovsky <orlovsky@pandoracore.com>
2019-11-24 00:32:07 +0200

    Working pubkey commitments

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # lbx: 
 
 Command-line tool for working with LNP/BP technology stack
+
+Sample commands:
+
+```bash
+# commits to the message by tweaking the public key according to LNPBP-1 standard:
+$ lbx pubkey_commit "The message" 02d1d80235fa5bba42e9612a7fe7cd74c6b2bf400c92d866f28d429846c679cceb
+```
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,46 @@
 #[macro_use] extern crate clap;
 
-use std::{io, fs, thread, env};
+use std::{io, fs, thread, env, str::FromStr};
+use clap::*;
+use secp256k1::PublicKey;
+use lbpbp::commitments::{base::*, secp256k1::*};
+use bitcoin::hashes::{sha256, HashEngine, Hash, hex::ToHex};
+use std::process::exit;
 
 fn main() -> io::Result<()> {
-    eprintln!("\nlbx: LNP/BP technology command-line utility\n");
-
-    let matches = clapp_app!(lbx =>
+    let matches = clap_app!(lbx =>
+        (@setting SubcommandRequiredElseHelp)
         (version: "0.1.0")
         (author: "Dr Maxim Orlovsky <orlovsky@pandoracore.com>")
         (about: "LNP/BP technology command-line utility")
-        ()
+        (@subcommand pubkey_commit =>
+            (about: "commits to the given MSG by tweaking provided public key")
+            (@arg MSG: +required "Source message to commit to")
+            (@arg PUBKEY: +required "Original public key for the tweak")
+        )
     ).get_matches();
 
+    match matches.subcommand() {
+        ("pubkey_commit", sm) => pubkey_commit(sm.unwrap()),
+        _ => (),
+    }
+
     Ok(())
+}
+
+fn pubkey_commit(matches: &ArgMatches) {
+    eprintln!("Committing to the message by tweaking the provided public key");
+
+    let msg = matches.value_of("MSG").unwrap();
+    let pubkey = PublicKey::from_str(matches.value_of("PUBKEY").unwrap()).unwrap_or_else(|_| {
+        eprintln!("The provided PUBKEY is invalid, exiting");
+        exit(1);
+    });
+    let mut tweaked_pubkey = pubkey.clone();
+
+    let engine = TweakEngine::new();
+    let digest = sha256::Hash::hash(msg.as_bytes());
+    let _ = engine.commit(&TweakSource(digest), &mut tweaked_pubkey);
+
+    println!("{}", tweaked_pubkey.to_hex());
 }
